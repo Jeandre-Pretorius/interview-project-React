f2f9a381131aef74c3719d631938d583289b3678
Jeandre Pretorius <jeandre@cubezoo.joburg>
2024-03-30 22:46:19 +0200

    API working

--- dummy-api/api.js
+++ dummy-api/api.js
@@ -2,19 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = 8008;
+const cors = require('cors'); // Import cors
+
+app.use(cors()); // Use cors middleware to enable CORS
 
 // Replace 'data.json' with the path to your JSON file
-const jsonDataPath = './json/ZA_Population.json';
+const ZA_Populations = './json/ZA_Population.json';
 
-app.get('/data', (req, res) => {
-    fs.readFile(jsonDataPath, 'utf8', (err, data) => {
+app.get('/ZA_populations', (req, res) => {
+    fs.readFile(ZA_Populations, 'utf8', (err, data) => {
         if (err) {
             res.status(500).send({ error: 'Failed to load data' });
             return;
         }
         try {
             const jsonData = JSON.parse(data);
-            res.json(jsonData); // This automatically sets Content-Type to application/json
+            res.json(jsonData);
         } catch (parseError) {
             res.status(500).send({ error: 'Failed to parse JSON data' });
         }
